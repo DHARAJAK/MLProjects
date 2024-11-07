8c020536957a856f60203b6cea852e39107df0fd
Dharmendra Rajak <dharmendrarajak11@gmail.com>
2024-11-07 21:00:33 +0530

    Model Trainer created

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -11,6 +11,11 @@ from dataclasses import dataclass
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
 
+
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
+
+
 @dataclass
 class DataIngestionConfig:
     train_data_path: str= os.path.join('artifacts', "train.csv")
@@ -25,7 +30,7 @@ class DataIngestion:
         logging.info("Entered the data ingestion method or component")
         try:
             df= pd.read_csv('notebook\data\stud.csv')
-            logging.info('Read the dataset as DataFram')
+            logging.info('Read the dataset as DataFrame')
 
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
 
@@ -56,4 +61,7 @@ if __name__== "__main__":
 
     data_transformation = DataTransformation()
 
-    data_transformation.initiate_data_transformation(train_data, test_data)
+    train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_data, test_data)
+
+    Modeltrainer = ModelTrainer()
+    print(Modeltrainer.initiate_model_trainer(train_arr, test_arr))
